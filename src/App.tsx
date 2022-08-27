7ae12d75b05b54f0a9b8dbb6ad3b496a5f058f63
Yan Campos <camposyan00@gmail.com>
2022-08-27 14:11:05 -0300

    feat: componente modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import Modal from "react-modal";
+
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 function App() {
+  const [isNewTransactionModalOpen, setNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -3,38 +3,37 @@ import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
-    useEffect(() => {
-        api.get('transactions')
-            .then(data => console.log(data))
-    }, []);
+  useEffect(() => {
+    api.get("transactions").then((response) => console.log(response.data));
+  }, []);
 
-    return(
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
 
-                <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$12.000,00</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/08/2022</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">- R$1.000,00</td>
-                        <td>Casa</td>
-                        <td>22/08/2022</td>
-                    </tr>
-                </tbody>
-            </table>
-        </Container>
-    );
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/08/2022</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$1.000,00</td>
+            <td>Casa</td>
+            <td>22/08/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
 }
