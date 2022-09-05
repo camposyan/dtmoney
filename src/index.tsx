78b543c935ac6fb2696195d48da9b0c20945f89b
Yan Campos <camposyan00@gmail.com>
2022-09-05 20:28:11 -0300

    feat: dados dinamicos na tabela e formatação de dados

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import App from './App';
 
 createServer({
-  routes() {
-    this.namespace = 'api';
+    models: {
+        transaction: Model,
+    },
 
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date() 
-        }
-      ]
-    })
-  }
+    seeds(server) {
+        server.db.loadData({
+            transactions: [
+                {
+                    id: 1,
+                    title: 'Freelance de website',
+                    type: 'deposit',
+                    category: 'Dev',
+                    amount: 6000,
+                    createdAt: new Date('2022-08-30 21:00:00')
+                },
+                {
+                    id: 2,
+                    title: 'Aluguel',
+                    type: 'withdraw',
+                    category: 'Casa',
+                    amount: 1100,
+                    createdAt: new Date('2022-08-22 13:00:00')
+                }
+            ]
+        })
+    },
+
+    routes() {
+        this.namespace = 'api';
+
+        this.get('/transactions', () => {
+            return this.schema.all('transaction')
+        })
+
+        this.post('/transactions', (schema, request) => {
+            const data = JSON.parse(request.requestBody)
+
+            return schema.create('transaction', data);
+        })
+    }
 })
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+    <App />
 );
